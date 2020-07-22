00e6c74b814aa0ceac2f5c082af3877d86f85639
Lucian nicolescu <lucian.nicolescu.cello@gmail.com>
2020-07-22 15:00:56 -0700

    merge

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,19 @@
 
 console.log('Does this work yet?')
 
+const isEmail = input => {
+    let inputEmail = String(input);
+    if (inputEmail) {
+      // let re = /[0-9]/;
+      // let re = /[0-9][0-9][0-9]/;
+      // let re = /^[0-9][0-9][0-9]$/;
+      // let re = /^\d\d\d$/;
+      let re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+      console.log(input)
+      return re.test(inputEmail);
+    }
+    return false;
+  }
 
 const validateInput = element => {
     let response = {
@@ -13,11 +26,13 @@ const validateInput = element => {
         element.value.length < 3 &&
         (element.name === 'name')
     ) {
-        response.message = 'Enter your full name';
-    } else if (element.value.length < 1 && element.name === 'email') {
-        response.message = 'Please enter a valid email';
+        alert('Enter your full name');
+    } else if (isEmail === false && element.name === 'email') {
+        alert('Please enter a valid email');
+        console.log('Please enter a valid email')
     } else {
         response.isValid = true;
+        alert("Thank you!")
     }
     return response;
 }
@@ -29,11 +44,11 @@ contactEl.addEventListener('submit', e => {
     let allInputValid = true;
     contactEl.querySelectorAll('input, textarea').forEach(el => {
         console.log('pizza');
-        //   let errorEl = el.parentElement.firstElementChild;
+        let errorEl = el.parentElement.firstElementChild;
         let validationResponse = validateInput(el);
         if (!validationResponse.isValid) {
             allInputValid = false;
-            el.classList.add('error');
+            // el.classList.add('error');
             // errorEl.classList.remove('hidden', 'opaque');
             // errorEl.innerText = validationResponse.message;
         } else {
@@ -52,11 +67,11 @@ contactEl.addEventListener('submit', e => {
     }
 })
 
-//   document.querySelectorAll('input, textarea').forEach(el => {
-//     el.addEventListener('focus', e => {
-//       el.parentElement.firstElementChild.classList.add('opaque');
-//     })
-//   })
+  document.querySelectorAll('input, textarea').forEach(el => {
+    el.addEventListener('focus', el => {
+      el.parentElement.firstElementChild.classList.add('opaque');
+    })
+  })
 
 
 
