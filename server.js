351b709d73b8d774d9e0d1d3b997396b86612d04
RommelAquino <rommel.h.aquino@gmail.com>
2020-07-21 19:19:39 -0700

    made schema

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const db = require('/models');
 app.set('view engine', 'ejs');
 
 //----- Middleware
@@ -14,12 +15,24 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
+app.post('/', (req, res) => {
+    console.log(req.body);
+    db.Users.create(req.body, (err, newUser) => {
+        if (err) return console.log(err);  
+        console.log(newUser);
+    });
+    res.redirect('/');
+});
+
 app.get('/dashboard', (req, res) => {
     res.render('dashboard');
-})
-
-
-
+    db.Users.find({}, (err, allUsers) => {
+        if (err) return console.log(err);  
+        res.render('dashboard', {
+          users: allUsers,
+        });       
+    });
+});
 
 //----- 404 Route
 
